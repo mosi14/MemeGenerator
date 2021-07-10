645a80ab0d9552fa5f5cd31827b6c939496f9b3b
@s289832 <s289832@studenti.polito.it>
2021-07-10 18:34:01 +0200

    add : different addmin

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,85 +1,21 @@
-// import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import Card from "react-bootstrap/Card";
-// import Button from "react-bootstrap/Button";
-// import Modal from "react-bootstrap/Modal";
-// import API from "../API";
 import Meme from "./meme";
 
 const Home = (props) => {
- 
   //const [showButton, setshowButton] = useState(false);
-  const memeList= props.memeList;
- console.log("memelist home:  " + props.memeList);
-
+  const memeList = props.memeList;
+  const isLoggedIn = props.isLoggedIn;
+  console.log("memelist home:  " + props.memeList);
 
   return (
     <>
       <Container fluid>
         <Row className="mt-5">
           {memeList.map((meme) => (
-            <Meme key={meme.id}  meme={meme}  />
+              <Meme key={meme.id} meme={meme} isLoggedIn={isLoggedIn}/>
           ))}
-
-          {/* <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="./images/2.jpg"
-                onClick={handlememeShow}
-                className="memeImage"
-              />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-
-                <Button variant="primary" className="mr-2">
-                  Copy
-                </Button>
-                <Button variant="danger">Delete</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="./images/3.jpg"
-                onClick={handlememeShow}
-                className="memeImage"
-              />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-
-                <Button variant="primary" className="mr-2">
-                  Copy
-                </Button>
-                <Button variant="danger">Delete</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="./images/4.jpg"
-                onClick={handlememeShow}
-                className="memeImage"
-              />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-
-                <Button variant="primary" className="mr-2">
-                  Copy
-                </Button>
-                <Button variant="danger">Delete</Button>
-              </Card.Body>
-            </Card>
-          </Col> */}
         </Row>
-
-
       </Container>
     </>
   );
--- client/src/components/meme.js
+++ client/src/components/meme.js
@@ -1,60 +1,88 @@
 import { Col, Button, Card, Modal } from "react-bootstrap";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import API from "../API";
 const Meme = (props) => {
   const [memeShow, setmemeShow] = useState(false);
+  const [currentUser, setCurrentUser] = useState([]);
   const handleMemeClose = () => setmemeShow(false);
   const handlememeShow = () => setmemeShow(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        // here you have the user info, if already logged in
+        // TODO: store them somewhere and use them, if needed
+        var currentUser = await API.getUserInfo();
+        console.log(currentUser);
+        setCurrentUser(currentUser);
+      } catch (err) {
+        console.error(err.error);
+      }
+    };
+    checkAuth();
+  }, []);
+
   return (
     <>
-    <Col md={3}>
-            <Card style={{ width: "18rem" }}>
-        <Card.Img
-          variant="top"
-          src={`./images/${props.meme.imgId}.jpg`} 
-         // onClick={handlememeShow}
-          className="memeImage"
-        />
-        <Card.Body>
-          <Card.Title>{props.meme.title}</Card.Title>
-          <Button variant="primary" className="mr-2" >
-            Copy
-          </Button>
-          <Button variant="danger">Delete</Button>
-        </Card.Body>
-      </Card>
-    </Col>
-    <Modal
-          show={memeShow}
-          onHide={handleMemeClose}
-          className="memeModal"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Card.Title>
-              Card Title
-              <span class="material-icons-outlined">account_circle</span>
-            </Card.Title>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src="./images/4.jpg"
-                className="memeModal_image"
-              />
-              <p className="memeModal_firstText">FIRST TITLE</p>
-              <p className="memeModal_secondText">Second TITLE</p>
-              <p className="memeModal_thirdText">Third TITLE</p>
-              {/* <Card.Body>
+      <Col md={3}>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={`./images/${props.meme.imgId}.jpg`}
+            // onClick={handlememeShow}
+            className="memeImage"
+          />
+          <Card.Body>
+            <Card.Title>{props.meme.title}</Card.Title>
+            {props.isLoggedIn ? (
+              <>
+                <Button variant="primary" className="mr-2">
+                  Copy
+                </Button>
+                {props.meme.userId === currentUser.id ? (
+                  <Button variant="danger">Delete</Button>
+                ) : (
+                  ""
+                )}
+              </>
+            ) : (
+              ""
+            )}
+          </Card.Body>
+        </Card>
+      </Col>
+      <Modal
+        show={memeShow}
+        onHide={handleMemeClose}
+        className="memeModal"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Card.Title>
+            Card Title
+            <span class="material-icons-outlined">account_circle</span>
+          </Card.Title>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="./images/4.jpg"
+              className="memeModal_image"
+            />
+            <p className="memeModal_firstText">FIRST TITLE</p>
+            <p className="memeModal_secondText">Second TITLE</p>
+            <p className="memeModal_thirdText">Third TITLE</p>
+            {/* <Card.Body>
                 
             
                 
               </Card.Body> */}
-            </Card>
-          </Modal.Body>
-          <Modal.Footer></Modal.Footer>
-        </Modal>
+          </Card>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
     </>
   );
 };
